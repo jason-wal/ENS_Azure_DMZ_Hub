622cfe8f3e47463e4e6f6eb7d773c85becf4c00f
io.jason.walters <io.jason.walters@usda.gov>
2026-08-06 09:36:50 -0500

    updated svc endpoint

--- main.tf
+++ main.tf
@@ -289,9 +289,18 @@ resource "azurerm_subnet" "this" {
     default_outbound_access_enabled               = each.value["default_outbound_access_enabled"]  
     private_link_service_network_policies_enabled = each.value["priv_link_net_pols"] 
 
-    service_endpoint {
+  /*  service_endpoint {
               service = contains( each.value["Sub_Svc_Endpoints"], "null" ) ? null : each.value["Sub_Svc_Endpoints"]
             }
+   */
+    
+    dynamic "service_endpoint" {
+      for_each = each.value["Sub_Svc_Endpoints"] != "null" ? each.value["Sub_Svc_Endpoints"] : []
+      content {
+        service = each.value["Sub_Svc_Endpoints"]
+      }
+    }
+
 
     dynamic "delegation" {
         for_each = each.value["Sub_Delegation"] ? [1] : [] 
